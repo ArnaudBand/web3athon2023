24a0b5f4a59a081123f3d531af23987183448aae
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-05-21 16:21:04 +0000

    Use Navbar as component

--- src/App.js
+++ src/App.js
@@ -1,49 +1,10 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import Header from "./components/Header";
 import "./styles/Home.css";
 
 export default function Home() {
   return (
-    <div className="container">
-      <main className="main">
-        <h1 className="text-3xl font-bold underline">
-          Nft's mint app
-        </h1>
-
-        <p className="description">
-          Get started by configuring your desired network in{" "}
-          <code className="code">src/index.js</code>, then modify the{" "}
-          <code className="code">src/App.js</code> file!
-        </p>
-
-        <div className="connect">
-          <ConnectWallet dropdownPosition={{ side: 'bottom', align: 'center' }} />
-        </div>
-
-        <div className="grid">
-          <a href="https://portal.thirdweb.com/" className="card">
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className="card">
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a href="https://portal.thirdweb.com/templates" className="card">
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
-      </main>
+    <div className="">
+        <Header />
     </div>
   );
 }
